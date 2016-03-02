3cd5141f72aa68cb5c447a1b0caf7d9e1470a143
Alejandro Forero Cuervo <alefore@gmail.com>
2016-03-02 01:03:37 +0100

    Added street.

--- murd.js
+++ murd.js
@@ -1,4 +1,6 @@
-var murd = {}
+var murd = {};
+
+murd.flags = { 'alarmClockOff': 'alarmClockOff' };
 
 murd.Bedroom = function() {
   this.NAME = 'bedroom';
@@ -6,11 +8,16 @@ murd.Bedroom = function() {
 };
 murd.Bedroom.prototype = new engine.Room();
 murd.Bedroom.prototype.Description = function(world) {
-  return 'Your bedroom could use some cleaning.<br>' +
-    'There\'s a toilet nearby.';
+  var description = "Your bedroom is a bit messy.";
+  if (!world.GetFlag('alarmClockOff')) {
+    description += " The annoying alarm clock is beeping loudly."
+  }
+  description +=
+      " Two doors are here, one leads to the toilet and one to the street.";
+  return description;
 };
 murd.Bedroom.prototype.Exits = function(world) {
-  return {'toilet': true}
+  return {'toilet': true, 'street': true}
 };
 murd.BEDROOM = new murd.Bedroom();
 
@@ -31,11 +38,33 @@ murd.Toilet.prototype.CanEnter = function(world) {
        'A foul smell penetrates your nostrils.');
    return true;
 };
-
 murd.Toilet.prototype.Exits = function(world) {
   return {'bedroom': true}
 };
 murd.TOILET = new murd.Toilet();
+
+murd.Street = function() {
+  this.NAME = 'street';
+  this.TITLE = 'the street';
+};
+murd.Street.prototype = new engine.Room();
+murd.Street.prototype.Description = function(world) {
+  return "There's a small park in front of your apartment (bedroom).";
+};
+murd.Street.prototype.CanEnter = function(world) {
+  if (!world.GetFlag(murd.flags.alarmClockOff)) {
+    world.Print('You should turn that stupid alarm clock off first.');
+    return false;
+  }
+  world.Print(
+      "You unlock the door and take the stairs down. The sun is shinning, "
+      + "it's a beautiful day.");
+  return true;
+};
+murd.Street.prototype.Exits = function(world) {
+  return {'bedroom': true};
+};
+murd.STREET = new murd.Street();
 
 murd.Airport = function() {
   this.NAME = 'airport';
@@ -55,19 +84,35 @@ murd.Game.prototype.InitState = function(world) {
 };
 
 murd.Game.prototype.HandleAction = function(world, verb, words) {
-  if (verb !== 'open' || words[0] !== 'door') {
-    return false;
+  if (verb == 'open') {
+    if (words[0] !== 'door') {
+      return false;
+    }
+    if (!('toilet' in world.location.Exits())) {
+      return false;
+    }
+    world.SetFlag('toiletDoorOpen', true);
+    world.Print('Ok');
+    return true;
   }
-  if (!('toilet' in world.location.Exits())) {
-    return false;
+
+  if (verb == 'turn' && words[0] == 'off') {
+    if (words[1] !== 'clock') {
+      return false;
+    }
+    if (world.location != murd.BEDROOM) {
+      return false;
+    }
+    world.SetFlag(murd.flags.alarmClockOff, true);
+    world.Print('Ahh, finally a bit of peace.');
+    return true;
   }
-  world.SetFlag('toiletDoorOpen', true);
-  world.Print('Ok');
-  return true;
+  return false;
 };
 
 murd.Game.prototype.ROOMS = [
   murd.BEDROOM,
   murd.TOILET,
+  murd.STREET,
   murd.AIRPORT,
 ];
