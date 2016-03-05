c8d5d0afd9ed6f1a9f375077b6cb0b28805969f5
Alejandro Forero Cuervo <alefore@gmail.com>
2016-03-05 16:42:52 +0100

    Add a few more rooms: office, bar, wiedikon, enge, hauptbahnhof.

--- murd.js
+++ murd.js
@@ -82,11 +82,13 @@ murd.RESTROOM = engine.MakeRoom({
   },
 });
 
+// TODO: Add a mailbox, with some stuff that becomes critical later in the game?
 murd.STREET = engine.MakeRoom({
   NAME: 'street',
   TITLE: 'the street',
   Description: function(world) {
-    return "There's a small park in front of your apartment (bedroom).";
+    return "You're in a small park. From here you can go back into your "
+           + "bedroom, to the local bar, or to the wiedikon train station.";
   },
   CanEnter: function(world) {
     world.Print(
@@ -95,14 +97,153 @@ murd.STREET = engine.MakeRoom({
     return true;
   },
   Exits: function(world) {
-    return {'bedroom': true};
+    return {'bedroom': true, 'wiedikon': true, 'bar': true};
   }
+});
+
+murd.BAR = engine.MakeRoom({
+  NAME: 'bar',
+  TITLE: 'the bar',
+  Description: function(world) {},
+  CanEnter: function(world) {
+    world.Print(
+        "The bar is currently closed. It'll only open in the afternoon.<br>"
+        + "You knock on the door repeatedly, but nobody opens. One woman walks "
+        + "by, shaking her head and muttering something in Swiss German, "
+        + "giving you a look of disapproval.");
+    return false;
+  },
+});
+
+function DescriptionTrainLines(room) {
+  var output =
+      "A few train lines run through here, taking you to the following "
+      + "stations:";
+  var separator = " ";
+  room.TrainLines().forEach(
+      function(name) { output += separator + name; separator = ", "; });
+  return output + "<br>";
+}
+
+function ExitsTrainLines(room, exits) {
+  room.TrainLines().forEach(
+      function(name) { exits[name.toLowerCase()] = true; });
+  return exits;
+}
+
+murd.WIEDIKON = engine.MakeRoom({
+  NAME: 'wiedikon',
+  TITLE: 'the Wiedikon train station',
+  TrainLines: function() {
+    return ["Enge", "Oerlikon", "Airport", "Hauptbahnhof"];
+  },
+  Description: function(world) {
+    return "You're in Wiedikon, a small train station.<br>"
+           + DescriptionTrainLines(murd.WIEDIKON)
+           + "The station has a big clock, always on time. Damn, unlike it, "
+           + "you're running late.";
+  },
+  CanEnter: function(world) {
+    world.Print("You enter the Wiedikon train station.");
+    return true;
+  },
+  CanLeave: function(world, toRoom) {
+    if (toRoom == murd.ENGE) {
+      return true;
+    }
+    world.Print(
+        "You don't have time for that: you need to get to work soon! Take a "
+        + "train to Enge, that's the ticket.");
+    return false;
+  },
+  Exits: function(world) {
+    return ExitsTrainLines(murd.WIEDIKON, {});
+  }
+});
+
+// TODO: Add a Kiosk inside the station?
+murd.ENGE = engine.MakeRoom({
+  NAME: 'enge',
+  TITLE: 'the Enge train station',
+  TrainLines: function() {
+    return ["Wiedikon", "Oerlikon", "Airport", "Hauptbahnhof"];
+  },
+  Description: function(world) {
+    return "You're at the beautiful Enge train station. This whole thing was "
+        + "built with granite from Ticino. Thousands of people commute here "
+        + "everyday, going through the imposing facade.<br>"
+        + DescriptionTrainLines(murd.ENGE)
+        + "From here you can reach your office easily.";
+  },
+  CanEnter: function(world) {
+    if (world.location == murd.WIEDIKON) {
+      world.Print(
+          "You hop on the S2 train to Enge. It's a bit crowded.<br>"
+          + "Once it reaches Enge, you make it back to the door and get off.");
+    } else {
+      // TODO: Add more custom messages.
+      world.Print("Ok.");
+    }
+    return true;
+  },
+  CanLeave: function(world, toRoom) {
+    if (toRoom == murd.OFFICE) {
+      return true;
+    }
+    if (toRoom == murd.WIEDIKON) {
+      world.Print("Why would you go back now? You didn't forget anything.");
+    } else {
+      world.Print(
+          "There's no time for that! You really should get to the office now.");
+    }
+    return false;
+  },
+  Exits: function(world) {
+    return ExitsTrainLines(murd.ENGE, {'office': true});
+  }
+});
+
+murd.HAUPTBAHNHOF = engine.MakeRoom({
+  NAME: 'hauptbahnhof',
+  TITLE: 'the main train station in Zurich',
+  Description: function(world) {},
 });
 
 murd.AIRPORT = engine.MakeRoom({
   NAME: 'airport',
   TITLE: 'the airport',
   Description: function(world) {},
+});
+
+murd.OERLIKON = engine.MakeRoom({
+  NAME: 'oerlikon',
+  TITLE: 'the oerlikon train station',
+  Description: function(world) {},
+});
+
+murd.OFFICE = engine.MakeRoom({
+  NAME: 'office',
+  TITLE: 'the office',
+  Description: function(world) {
+    return "Your office, in a small branch of a big bank.<br>"
+           + "<h1>You won the game!</h1>";
+  },
+  CanEnter: function(world) {
+    world.Print(
+        "You go through the door, take the lift an enter your cubicle. Looks "
+        + "like nobody noticed how late you are. You sit down on your chair in "
+        + "front of the computer."
+        + "<br>"
+        + "<h1>You won the game!</h1>");
+    return true;
+  },
+  CanLeave: function(world, toRoom) {
+    world.Print("No, you need to focus on your work!");
+    return false;
+  },
+  Exits: function(world) {
+    return {'enge': true};
+  }
 });
 
 murd.Game = function() {
@@ -168,5 +309,11 @@ murd.Game.prototype.ROOMS = [
   murd.BEDROOM,
   murd.RESTROOM,
   murd.STREET,
+  murd.BAR,
+  murd.WIEDIKON,
+  murd.ENGE,
+  murd.HAUPTBAHNHOF,
   murd.AIRPORT,
+  murd.OERLIKON,
+  murd.OFFICE,
 ];
